702d6b1414d2536e6269de944fcb769ecd164838
Mariusz Malec <mario106c@gmail.com>
2022-10-19 21:09:25 +0200

    add command homeview and customerview

--- WPF_LoginForm/ViewModel/MainViewModel.cs
+++ WPF_LoginForm/ViewModel/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using FontAwesome.Sharp;
+using System.Threading;
+using System.Windows.Input;
 using WPF_LoginForm.Model;
 using WPF_LoginForm.Repositories;
 
@@ -8,6 +10,13 @@ namespace WPF_LoginForm.ViewModel
     {
         //fields
         private UserAccountModel _currentuserAccount{ get; set; }
+
+        //https://youtu.be/76JLBZJR5gE?t=1002
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
+
         private IUserRepository _userRepository;
 
         public UserAccountModel CurrentUserAccount
@@ -16,11 +25,52 @@ namespace WPF_LoginForm.ViewModel
             set { _currentuserAccount = value; OnPropertyChanged(nameof(CurrentUserAccount)); }
         }
 
+        public ViewModelBase CurrentChildView 
+        {
+            get { return _currentChildView; }
+            set { _currentChildView = value; OnPropertyChanged(nameof(CurrentChildView)); }
+        }
+        public string Caption 
+        {
+            get { return _caption; }
+            set { _caption = value; OnPropertyChanged(nameof(Caption)); }
+        }
+        public IconChar Icon 
+        {
+            get { return _icon; }
+            set { _icon = value; OnPropertyChanged(nameof(Icon)); }
+        }
+
+        //commands
+        public ICommand ShowHomeViewCommand { get;}
+        public ICommand ShowCustomerViewCommand { get; }
+
         public MainViewModel()
         {
             _userRepository = new UserRepository();
             CurrentUserAccount = new UserAccountModel();
+
+            //initialize commands
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
+
+            //Default view
+            ExecuteShowHomeViewCommand(null);
+
             LoadCurrentUserData();
+        }
+
+        private void ExecuteShowCustomerViewCommand(object obj)
+        {
+            CurrentChildView = new CustomerViewModel();
+            Caption = "Customers";
+            Icon = IconChar.UserGroup;
+        }
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            Caption = "Dashboard";
+            Icon = IconChar.Home;
         }
 
         private void LoadCurrentUserData()
